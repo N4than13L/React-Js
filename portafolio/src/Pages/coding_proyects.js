e6ebceaa9ed40dd5b4510bda620bcb0b827075f7
= <=>
2021-05-03 21:31:08 -0400

    agregando slider de imagenes para proyectos de programacion

--- portafolio/src/Pages/coding_proyects.js
+++ portafolio/src/Pages/coding_proyects.js
@@ -1,6 +1,26 @@
 import React,{Component} from 'react'
 import NavBar from '../components/NavBar'
 import Estudiantes from '../Img/Estudiantes.jpg'
+import '../App.css'
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css"
+
+//import "./styles.css";
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Navigation
+} from 'swiper/core';
+import Button_Back from '../components/Button_Back';
+
+// install Swiper modules
+SwiperCore.use([Navigation]);
+
 
 class Csharp_proyects extends Component {
     constructor(props){
@@ -11,14 +31,22 @@ class Csharp_proyects extends Component {
         return (
             <section style={{margin: 0, padding: 0, width: '100%'}}>
                 <NavBar/>
-                <div  style={{margin: 25, padding: 25, width: '95%'}}>
-                    <br/>
-                    <div className="box">
-                    <p className="title"></p>
-                        <img src={Estudiantes} style={{width: '100%'}}/>
-                    <p style={{fontSize: 25}}>{this.state.value = 'aplicacion de tareas'}</p>
-                    <p style={{fontSize:20}}></p>
-                </div> 
+                <div style={{margin: 69}}></div>
+                <Button_Back/>
+                
+                <div className="box">
+                <Swiper navigation={true} className="mySwiper">
+                    <SwiperSlide><img src={Estudiantes}/></SwiperSlide>
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                    <SwiperSlide>Slide 5</SwiperSlide>
+                    <SwiperSlide>Slide 6</SwiperSlide>
+                    <SwiperSlide>Slide 7</SwiperSlide>
+                    <SwiperSlide>Slide 8</SwiperSlide>
+                    <SwiperSlide>Slide 9</SwiperSlide>
+                </Swiper>
+                <p className="title">{this.state.value = 'Descripcion del proyecto'}</p>
                 </div>
                 
             </section>
---
+++ portafolio/src/components/Button_Back.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { BiArrowBack } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
+
+class Button_Back extends Component {
+    render() {
+        return(
+            <div style={{padding: 20}}>  
+                <Link to="/Coding" className="button is-info">
+                    <BiArrowBack/>
+                </Link>
+            </div>
+        )
+    }
+}
+
+export default Button_Back
